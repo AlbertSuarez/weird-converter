9528b237c3142477a2f3cf904d4c88a0f4a1d2bf
Albert Suarez <alsumo95@gmail.com>
2020-05-23 21:15:11 +0200

    Added setup script

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+
+with open('README.md', 'r') as readme_file:
+    long_description = readme_file.read()
+
+setuptools.setup(
+    name='weird-converter',
+    version='0.1',
+    author='AlbertSuarez',
+    author_email='alsumo95@gmail.com',
+    description='Strange combinations converter',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/AlbertSuarez/weird-converter',
+    license='Apache-2.0',
+    packages=['weird_converter'],
+    install_requires=[
+        'numpy==1.18.4',
+        'Pillow==7.1.2',
+        'scipy==1.4.1'
+    ],
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    keywords='converter audio image audio-to-image',
+)
