479d3fdac59676979c0766c9693e9b23fcb8e762
Albert Suarez <alsumo95@gmail.com>
2020-05-23 20:55:32 +0200

    Added silent.wav example

--- weird_converter/config.py
+++ weird_converter/config.py
@@ -4,5 +4,5 @@ TEST_FOLDER_DATA: str = 'data'
 TEST_FOLDER_EXAMPLES: str = 'examples'
 TEST_FOLDER_AUDIO: str = 'from_audio'
 TEST_FILE_AUDIO: str = 'baby_elephant.wav'
-TEST_FILE_AUDIO_LIST: list = [TEST_FILE_AUDIO, 'cantina_short.wav', 'fanfare.wav']
+TEST_FILE_AUDIO_LIST: list = [TEST_FILE_AUDIO, 'cantina_short.wav', 'fanfare.wav', 'silent.wav']
 TEST_FILE_IMAGE: str = f'image.{EXTENSION_IMAGE}'
