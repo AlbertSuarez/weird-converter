ee14baf7e5f41b3abab6815645b18fe2932ba2e6
Albert Suarez <alsumo95@gmail.com>
2020-05-24 12:51:44 +0200

    Added just black image

--- weird_converter/config.py
+++ weird_converter/config.py
@@ -8,4 +8,4 @@ TEST_FOLDER_IMAGE: str = 'from_image'
 TEST_FILE_AUDIO: str = 'baby_elephant.wav'
 TEST_FILE_AUDIO_LIST: list = [TEST_FILE_AUDIO, 'cantina_short.wav', 'fanfare.wav', 'silent.wav']
 TEST_FILE_IMAGE: str = f'church.{EXTENSION_IMAGE}'
-TEST_FILE_IMAGE_LIST: list = [TEST_FILE_IMAGE, 'goose.jpg', 'lighthouse.jpg', 'person.jpg', 'raven.jpg']
+TEST_FILE_IMAGE_LIST: list = [TEST_FILE_IMAGE, 'goose.jpg', 'lighthouse.jpg', 'person.jpg', 'raven.jpg', 'black.jpg']
