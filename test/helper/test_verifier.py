393fb2ef6ba5c4b6f65853960d28afb321c97400
Albert Suarez <alsumo95@gmail.com>
2020-05-24 12:13:37 +0200

    Added is_image verification

--- test/helper/test_verifier.py
+++ test/helper/test_verifier.py
@@ -1,21 +1,21 @@
 import os
 import unittest
 
-from weird_converter.config import TEST_FOLDER_EXAMPLES, TEST_FILE_AUDIO, TEST_FOLDER_DATA, TEST_FOLDER_AUDIO, \
-    TEST_FILE_IMAGE
+from weird_converter.config import TEST_FOLDER_EXAMPLES, TEST_FILE_AUDIO, TEST_FOLDER_AUDIO, TEST_FILE_IMAGE, \
+    TEST_FOLDER_IMAGE
 from weird_converter.helper import verifier
 
 
 class HelperVerifierTest(unittest.TestCase):
 
     def setUp(self):
-        self.existing_file_path: str = os.path.join(TEST_FOLDER_EXAMPLES, TEST_FOLDER_AUDIO, TEST_FILE_AUDIO)
-        self.non_existing_file_path: str = os.path.join(TEST_FOLDER_DATA, TEST_FILE_AUDIO)
+        self.audio_file_path: str = os.path.join(TEST_FOLDER_EXAMPLES, TEST_FOLDER_AUDIO, TEST_FILE_AUDIO)
+        self.image_file_path: str = os.path.join(TEST_FOLDER_EXAMPLES, TEST_FOLDER_IMAGE, TEST_FILE_IMAGE)
+        self.non_existing_file_path: str = os.path.join(TEST_FOLDER_EXAMPLES, TEST_FILE_AUDIO)
         self.directory_path: str = TEST_FOLDER_EXAMPLES
-        self.non_audio_file_path: str = os.path.join(TEST_FOLDER_DATA, TEST_FILE_IMAGE)
 
     def test_must_file_exist_success(self):
-        verifier.must_file_exist(self.existing_file_path)
+        verifier.must_file_exist(self.audio_file_path)
         self.assertTrue(True)
 
     def test_must_file_exist_error(self):
@@ -23,7 +23,7 @@ class HelperVerifierTest(unittest.TestCase):
             verifier.must_file_exist(self.non_existing_file_path)
 
     def test_must_not_directory_exist_success(self):
-        verifier.must_not_directory_exist(self.existing_file_path)
+        verifier.must_not_directory_exist(self.audio_file_path)
         self.assertTrue(True)
 
     def test_must_not_directory_exist_error(self):
@@ -31,9 +31,17 @@ class HelperVerifierTest(unittest.TestCase):
             verifier.must_not_directory_exist(self.directory_path)
 
     def test_is_audio_success(self):
-        verifier.is_audio(self.existing_file_path)
+        verifier.is_audio(self.audio_file_path)
         self.assertTrue(True)
 
     def test_is_audio_error(self):
         with self.assertRaises(ValueError):
-            verifier.is_audio(self.non_audio_file_path)
+            verifier.is_audio(self.image_file_path)
+
+    def test_is_image_success(self):
+        verifier.is_image(self.image_file_path)
+        self.assertTrue(True)
+
+    def test_is_image_error(self):
+        with self.assertRaises(ValueError):
+            verifier.is_image(self.audio_file_path)
--- weird_converter/helper/verifier.py
+++ weird_converter/helper/verifier.py
@@ -1,5 +1,7 @@
 import os
 
+from PIL import Image, UnidentifiedImageError
+
 from weird_converter.config import EXTENSION_AUDIO
 
 
@@ -31,3 +33,17 @@ def is_audio(path: str):
     """
     if os.path.splitext(os.path.basename(path))[-1].lower() != f'.{EXTENSION_AUDIO}':
         raise ValueError(f'File with path [{path}] is not a {EXTENSION_AUDIO} type of file.')
+
+
+def is_image(path: str):
+    """
+    Checks if a given path points to a valid image file.
+    :param path: Given path.
+    :return: True if the path points to an image file, False otherwise.
+    """
+    try:
+        image = Image.open(path)
+        image.verify()
+        image.close()
+    except UnidentifiedImageError:
+        raise ValueError(f'File with path [{path}] is not a image type of file.')
